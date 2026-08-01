bd8a6debfefe2a102e779f869baa80deef09c1ad
Parsa Ghahremani <parsa.gh312.parsa@gmail.com>
2026-08-01 11:10:36 +0330

    complete header and navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Header from "./components/header/Header";
+import Hero from "./components/HeroSection/Hero";
 
 export default function Home() {
   return (
-
+    <>
       <Header />
 
-
+      <main>
+         <Hero />          {/* اینجا بعداً Hero اصلی سایتت را می‌نویسی */}
+      </main>
+    </>
   );
 }
